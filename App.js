d14acf192757510f87629f10b15efa0c38b759bf
Aayush Kumar <aayushkr1712@gmail.com>
2024-05-23 13:03:29 +0530

    first react

---
+++ App.js
@@ -0,0 +1,22 @@
+const parent = React.createElement("div", {id:"parent"}, [
+               React.createElement("div", {id:"child1"},[
+               React.createElement("h1", {}, "I am an H1 tag"),
+               React.createElement("h2", {}, "I am an H2 tag")]),
+
+                React.createElement("div", {id:"child2"},[
+                React.createElement("h1", {}, "I am an H1 tag"),
+                React.createElement("h2", {}, "I am an H2 tag")])
+            ]);
+
+console.log(parent);
+
+
+const heading = React.createElement("h1", 
+{id:"heading", xyz:"abc"}, "Hello World from React");
+
+// console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(parent);
